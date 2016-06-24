dbd280859198b23f9530f55931d3b9df9d69313c
Oleg Obleukhov <oleg.obleukhov@innogames.com>
2016-06-24 18:14:06 +0200

    Added missing fil

---
+++ access.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"database/sql"
+	"os"
+)
+
+type Access struct {
+	Username     string
+	Password     string
+	InitPassword string
+	Host     string
+	Port     int
+}
+
+type AccessConfig struct {
+	Access Access
+}
+
+func getAccessFromConfig() (Access, error) {
+	var accessConf AccessConfig
+	if _, err := toml.DecodeFile("/etc/mmdu/mmdu.toml", &accessConf); err != nil {
+		return accessConf.Access, err
+	}
+	return accessConf.Access, nil
+}
+
+func (a *Access) getConnectionString(initPass bool) string {
+	if a.Username == "" {
+		a.Username = "root"
+	}
+	if a.Host == "" {
+		a.Host = "localhost"
+	}
+	if a.Port == 0 {
+		a.Port = 3306
+	}
+
+	if initPass {
+		if a.InitPassword == "" {
+			return fmt.Sprintf("%s@tcp(%s:%d)/", a.Username, a.Host, a.Port)
+		} else {
+			return fmt.Sprintf("%s:%s@tcp(%s:%d)/", a.Username, a.InitPassword, a.Host, a.Port)
+		}
+	} else {
+		if a.InitPassword == "" {
+			return fmt.Sprintf("%s@tcp(%s:%d)/", a.Username, a.Host, a.Port)
+		} else {
+			return fmt.Sprintf("%s:%s@tcp(%s:%d)/", a.Username, a.Password, a.Host, a.Port)
+		}
+	}
+
+}
+
+func (a *Access)connectAndCheck() *sql.DB {
+	db, err := sql.Open("mysql", a.getConnectionString(true))
+	if err != nil {
+		fmt.Println("Unable to connect to mysql", err.Error())
+		os.Exit(2)
+	}
+
+	// Check if we can access database with initPass
+	err = db.Ping()
+	if err != nil {
+		db, err = sql.Open("mysql", a.getConnectionString(false))
+		if err != nil {
+			fmt.Println("Unable to connect to mysql", err.Error())
+		}
+
+		// Check if we can access database with regular pass
+		err = db.Ping()
+		if err != nil {
+			fmt.Println("Can not connect to mysql with both passwords", err.Error())
+			os.Exit(2)
+		}
+	}
+
+	return db
+}
